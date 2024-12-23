b37cfc4e95aa83ba1237f21080d91fc32fe155db
albertquirky <teiladnam@gmail.com>
2024-12-23 03:38:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type('html').send(meow));
 
 const server = app.listen(port, () => console.log('Example app listening on port ${port}!'));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-var html = "hi";
+var meow = "hi";
 const fs = require('node:fs');
 
-fs.readFile('index2.html', 'utf8', (err, data) => { html = data; });
+fs.readFile('index2.html', 'utf8', (err, data) => { meow = data; });
