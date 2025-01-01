708c261b072e63f74b4c8e9fa356c2488fc25c41
albertquirky <teiladnam@gmail.com>
2025-01-01 08:13:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 10000;
 const util = require('util'); 
   
 const fs = require('node:fs');
