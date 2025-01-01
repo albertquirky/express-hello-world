9516f3ef100a31dea5a391e1692c0f410863d185
albertquirky <teiladnam@gmail.com>
2025-01-01 06:59:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,69 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const util = require('util'); 
+  
+const fs = require('node:fs');
 
+const jsonfile ="data - Copy.json";
 
-const server = app.listen(port, () => console.log('Example app listening on port ${port}!'));
+const server = app.listen(port, () => console.log('Example app listening on port '+port));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-var meow = "hi";
-const fs = require('node:fs');
+app.get("/", (req, res) => loadPage(res));
 
-fs.readFile('index2.html', 'utf8', (err, data) => { 
-  
-  
-  app.get("/", (req, res) => res.type('html').send(data));
-
-
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
-  res.send(req.res)
+		
+  console.log('Received POST data:', req.body);
+  fs.readFile(jsonfile, 'utf8', (err, data) => {
+	  let d = JSON.parse(data);
+	  if (req.body.hasOwnProperty("DeleteYes")) {
+	  	  let toDie = req.body["DeleteYes"];
+		  console.log("Killing "+toDie);
+		  for (let i = 0; i < d["Sins"].length; i++) {
+			  if (d["Sins"][i]["Name"] == toDie) 
+				  d["Sins"].splice(i,1);
+		  }
+	  }
+	  else if (req.body.hasOwnProperty("editing")) {
+		  for (let i = 0; i < d["Sins"].length; i++) {
+			  if (d["Sins"][i]["Name"] == req.body.Name) {
+			  	  console.log("hi");
+			  	  for (const k in d["Sins"][i]) {
+			  	  	  d["Sins"][i][k] = req.body[k];
+			  	  	  console.log(k+": "+d["Sins"][i][k] +"="+ req.body[k]);
+			  	  }
+			  }
+		  }
+	  }
+	  else {  		  
+			  d["Sins"].unshift(req.body);
+	  }
+	  fs.writeFile(jsonfile, JSON.stringify(d), (error)=>{});
+	  console.log("wrote to " + jsonfile + ": " +JSON.stringify(d));
+	  loadPage(res);
+	  });
 });
+
+
+function loadPage(res) {
+	
+	fs.readFile(jsonfile, 'utf8', (err, monsterData) => {
+			
+		fs.readFile('index2.html', 'utf8', (err, pageData) => {
+				
+			if (!monsterData) {
+				console.log("AAAAAAAAA!!!");
+				loadPage(res);				
+			}
+			else res.type('html').send(pageData.replace("{*}", monsterData));
+				
+		});
+		
+	});	
+
+}
